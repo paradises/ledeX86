61710372a005b9cd234bc44688358fb60ef14f05
paradises <paradises@users.noreply.github.com>
2021-03-07 13:00:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,3 +16,4 @@
 cd package/lean  
 rm -rf luci-theme-argon  
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+git clone https://github.com/esirplayground/luci-app-poweroff.git

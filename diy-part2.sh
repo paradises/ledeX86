84bdef80c699df745694b3d8117fabf929ccebe7
paradises <paradises@users.noreply.github.com>
2021-03-09 08:17:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,7 +13,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.7.1/g' package/base-files/files/bin/config_generate
 # Change default password
-sed -i 's/root::0:0:99999:7:::/root:$1$NwwB73JL$ImETkcL9hs157WVLbrVvT.:18694:0:99999:7:::/g' /etc/shadow
+#sed -i 's/root::0:0:99999:7:::/root:$1$NwwB73JL$ImETkcL9hs157WVLbrVvT.:18694:0:99999:7:::/g' /etc/shadow
 
 cd package/lean  
 rm -rf luci-theme-argon  

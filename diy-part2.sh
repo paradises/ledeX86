a7d54eca34a19f5794b35b65493da7cabad31a52
paradises <paradises@users.noreply.github.com>
2021-03-10 16:04:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.7.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.7.2/g' package/base-files/files/bin/config_generate
 # Change default password
 #sed -i 's/root::0:0:99999:7:::/root:$1$NwwB73JL$ImETkcL9hs157WVLbrVvT.:18694:0:99999:7:::/g' /etc/shadow
 
